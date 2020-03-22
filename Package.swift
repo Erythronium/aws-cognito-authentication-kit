08c0afa5deca97c30a23aa7f1e6146a34edbc2ff
Alex little <alex@reelapp.co.uk>
2020-03-22 16:47:48 +0000

    support ios

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "aws-cognito-authentication-kit",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v10_15), .iOS(.v13_0)],
     products: [
         .library(name: "AWSCognitoAuthenticationKit", targets: ["AWSCognitoAuthenticationKit"]),
     ],
